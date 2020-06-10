27e5b01d590a5c69f9e3bb3558ece5538dc163c7
Spencer Wood <spencer.wood@me.com>
2020-06-09 22:19:57 -0500

    added the bootstrap css to the index.js file, imported bootstrap npm package, added the Nav and Footer folders/files into the application

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { InputGroup, InputGroupText, InputGroupAddon, Input} from 'reactstrap';
 
-const SearchBar = (props) -=> {
+const SearchBar = (props) => {
     return (
-        
-    )
-}
+        <div>
+            <InputGroup>
+                <InputGroupAddon addonType='prepend'>
+                    <InputGroupText>Search</InputGroupText>
+                </InputGroupAddon>
+                <Input />
+            </InputGroup>
+        </div>
+    );
+};
+
+export default SearchBar;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchBar from '../../components/SearchBar';
 
 function Home(){
     return(
         <div>
             <h1>Home Heading</h1>
+            <SearchBar />
         </div>
     )
 }
