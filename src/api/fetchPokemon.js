3b1b5f9ffcaf71f96ea8697501c89b8a793f1f3c
Spencer Wood <spencer.wood@me.com>
2020-10-29 22:03:25 -0500

    added comments to the fetchPokemon file in the api folder

--- src/api/fetchPokemon.js
+++ src/api/fetchPokemon.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-
+// function to query the pokemon api site and pass the query from the app.js file to the search
+// then passes the data to the app.js file
 export const fetchPokemon = async (query) => {
     const {data} = await axios.get(
         `https://pokeapi.co/api/v2/pokemon/${query}`
